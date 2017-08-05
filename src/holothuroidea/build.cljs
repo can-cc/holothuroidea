b8d2ee93ddbb4c1821951a00928354e5ad1ad5ef
fwchen <crystal.tyan@gmail.com>
2017-08-06 00:39:54 +0800

    initial commit

--- src/holothuroidea/build.cljs
+++ src/holothuroidea/build.cljs
@@ -11,15 +11,6 @@
 (def fs (node/require "fs"))
 (def npath (node/require "path"))
 
-;; EXP map 里面不能 println
-(defn build-tree! [rest]
-  (let [input (first rest)
-        output (second rest)
-        parsed (parse-path input)]
-    (check-output-and-mkdir! output)
-    (->> parsed
-         (mapv #(write-category-data output %)))))
-
 (defn write-category-data [output category-data]
   (util/write-file! (.join npath output (str (:name category-data) ".json"))
                     (:articles category-data)))
@@ -27,6 +18,8 @@
 (defn check-output-and-mkdir! [output]
   (when (not (util/exist? output))
     (util/mkdir! output)))
+
+;; EXP map 里面不能 println
 
 (defn parse-md-file [path]
   (md->html (util/read-file path))) 
@@ -45,3 +38,11 @@
              :path (.join npath source-path name)
              :articles (parse-path-all-articles (.join npath source-path name))
              })))))
+
+(defn build-tree! [rest]
+  (let [input (first rest)
+        output (second rest)
+        parsed (parse-path input)]
+    (check-output-and-mkdir! output)
+    (->> parsed
+         (mapv #(write-category-data output %)))))
