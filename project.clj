2a04892e0f56e9a1d7e25641ac00da030f325170
fwchen <crystal.tyan@gmail.com>
2017-08-18 14:34:18 +0800

    add lein replace support

--- project.clj
+++ project.clj
@@ -1,14 +1,17 @@
 (defproject holothuroidea "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[com.cemerick/piggieback "0.2.1"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.671"]
+                 [cider/cider-nrepl "0.15.0"]
                  [org.clojure/core.async "0.3.426"]
                  [funcool/promesa "1.8.0"]
                  [fipp "0.6.8"]
                  [markdown-clj "0.9.99"]
                  [io.aviso/pretty "0.1.34"]
                  [lein-doo "0.1.7"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :jvm-opts ^:replace ["-Xmx1g"]
   :plugins [[lein-npm "0.6.2"]
             [io.aviso/pretty "0.1.34"]]
