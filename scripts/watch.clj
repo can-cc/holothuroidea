be781bf9db30a326b8249e29d82eca610a3f178b
fwchen <crystal.tyan@gmail.com>
2018-03-12 17:32:16 +0800

    同步了一下 watch 跟 build script

--- scripts/watch.clj
+++ scripts/watch.clj
@@ -4,8 +4,10 @@
   {:main 'holothuroidea.core
    :output-to "out/holothuroidea.js"
    :output-dir "out"
+   :optimizations :simple
    :target :nodejs
    :verbose true
    :foreign-libs [{:file "src"
                    :module-type :es6}] ;; or :commonjs / :amd
+   :cache-analysis true
    :pretty-print true})
